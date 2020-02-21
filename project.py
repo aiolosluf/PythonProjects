1c0ebe5babc1c10fce7b75ebb00df6b1797525df
aiolosluf <aiolos201314@gmail.com>
2020-02-21 23:15:19 +1300

    init

---
+++ project.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import pandas_profiling
+
+data = pd.read_csv('titanic.csv')
+profile = data.profile_report(title='Titanic Dataset')
+profile.to_file(output_file='result/titanic_report.html')
