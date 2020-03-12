5be6611d2ec93d8fbea5fb81ae74752a41cfd047
aiolosluf <aiolos201314@gmail.com>
2020-03-12 15:44:54 +1300

    cosmic data

--- project.py
+++ project.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import pandas_profiling
 
-data = pd.read_csv('titanic.csv')
-profile = data.profile_report(title='Titanic Dataset')
-profile.to_file(output_file='result/titanic_report.html')
+# data = pd.read_csv('titanic.csv')
+# profile = data.profile_report(title='Titanic Dataset')
+# profile.to_file(output_file='result/titanic_report.html')
+
+
+data = pd.read_csv('cosmic_feb.csv')
+profile = data.profile_report(title='Cosmic figures Feb')
+profile.to_file(output_file='result/cosmic_feb.html')
